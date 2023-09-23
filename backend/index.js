957af0f7354ebdbae4d4b5cd09240b712ce4bd8e
mendacium <sakshamlinkedin@gmail.com>
2023-09-23 12:15:48 +0530

    setting up react and express

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 5000
+
+app.use(express.json())
+
+app.use(cors())
+
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/profiles',require('./routes/profiles'))
+app.use('/api/profiles',require('./routes/profiles'))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello saksham!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
