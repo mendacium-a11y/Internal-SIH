b5f8046b7037790722828e850d91f8f62ef1d321
mendacium <sakshamlinkedin@gmail.com>
2023-09-23 13:52:06 +0530

    created the schema for the db

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/profiles',require('./routes/profiles'))
-app.use('/api/profiles',require('./routes/profiles'))
+app.use('/api/review',require('./routes/review'))
 
 
 
